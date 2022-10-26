0b381ce8a097d9a41631717a740e82c28b536d50
NEIL-smtg <sunghui2000@hotmail.com>
2022-10-26 21:31:09 +0800

    main.c
    Added main file and test file

---
+++ main.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: suchua <suchua@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/13 15:50:30 by suchua            #+#    #+#             */
+/*   Updated: 2022/10/20 20:02:23 by suchua           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int	main(void)
+{
+	char	*line;
+	int		i;
+	int		fd1;
+	int		fd2;
+	int		fd3;
+
+	fd1 = open("test.txt", O_RDONLY);
+	fd2 = open("text2.txt", O_RDONLY);
+	fd3 = open("text3.txt", O_RDONLY);
+	i = 1;
+	while (i <= 23)
+	{
+		line = get_next_line(fd1);
+		printf("line [%02d]: %s", i, line);
+		free(line);
+		// line = get_next_line(fd2);
+		// printf("line [%02d]: %s\n", i, line);
+		// free(line);
+		// line = get_next_line(fd3);
+		// printf("line [%02d]: %s\n", i, line);
+		// free(line);
+		i++;
+	}
+	close(fd1);
+	close(fd2);
+	close(fd3);
+	return (0);
+}
